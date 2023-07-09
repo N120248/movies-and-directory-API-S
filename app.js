4be94c89d6caf2557a755b2fa367d0faa6b37a48
radhika <radhikalanke123@gmail.com>
2023-07-09 08:06:44 +0000

    crud related Api's for movie and directory tables

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/players/", async (request, response) => {
+  const players = `select * from cricket_team`;
+  const tables = await db.all(players);
+  response.send(tables);
+});
+
+module.exports = app;
